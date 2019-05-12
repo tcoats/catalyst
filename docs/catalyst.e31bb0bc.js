4669b0f37a5118a11b99e1f76fd677a4e0c8d760
Thomas Coats <github@voodoolabs.net>
2019-05-13 10:07:20 +1200

    Build

--- docs/catalyst.e31bb0bc.js
+++ docs/catalyst.e31bb0bc.js
@@ -13760,7 +13760,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50751" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59155" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
